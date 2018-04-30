4212ea9bf61731f020267fc65ebd11f5825d8f4d
Sherry Xu <sens_x@Sherrys-MacBook-Pro.local>
2018-05-01 09:51:56 +1000

    statr notToBeFriendsException

--- MiniNet/src/Exceptions/NotToBeFriendsException.java
+++ MiniNet/src/Exceptions/NotToBeFriendsException.java
@@ -1,5 +1,16 @@
 package Exceptions;
 
+import javafx.scene.control.Alert;
+
 public class NotToBeFriendsException extends Exception{
 
+public void notToBeFriendsException()
+{
+
+	Alert alert = new Alert(Alert.AlertType.WARNING);
+	alert.setTitle("MESSAGES");
+	alert.setHeaderText("WARNING!");
+	alert.setContentText("The person is already existing, please check the name and fill agin.");
+	alert.show();
+}
 }
