325ac7c0b9eacbf7618010af2d40518d46d05b3d
Sherry Xu <sens_x@Sherrys-MBP.gateway>
2018-05-12 00:54:45 +1000

    tiny change

--- MiniNet/src/relations/Couple.java
+++ MiniNet/src/relations/Couple.java
@@ -26,14 +26,14 @@ public class Couple implements RelationManipulator{
 		// TODO Auto-generated method stub
 		if(!(partner instanceof Adult))
 		{
-			System.out.println("llllllll");
+		
 			throw new NotToBeCoupledException(partner);
 		}
 		else if(this.partner.getRelationship().containsKey("couple")) {
 			throw new NoAvailableException(partner);
 		}
 		else {
-			System.out.println("kkkkkk");
+		
 		selectPerson.getRelationship().get("couple").add(partner);
 		partner.getRelationship().put("couple", new ArrayList<Person>());
 		partner.getRelationship().get("couple").add(selectPerson);
@@ -43,26 +43,9 @@ public class Couple implements RelationManipulator{
 	@Override
 	public void remove() {
 		// TODO Auto-generated method stub
-//		selectPerson.getRelationship().remove("couple");
+
 		partner.getRelationship().remove("couple");
 
-//		if(selectPerson.getRelationship().containsKey("child"))
-//		{
-//		
-//			for(Person child:selectPerson.getRelationship().get("child")) {
-//				
-//				for(String relationType: child.getRelationship().keySet()) {
-//					
-//					for(Person childRelation: child.getRelationship().get(relationType)) {
-//						System.out.println("inside "+child.getName()+" "+relationType+" "+childRelation.getName());
-//						child.removeRelationship(relationType, childRelation);
-//					}
-//				}
-////				System.out.println("inside "+child.getName());
-//				
-//			}
-////			partner.getRelationship().remove("child");
-//		}
 	}
 
 }
